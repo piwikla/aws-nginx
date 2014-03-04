340ff95340e16ab184cecca887429b68bb9a4f10
piwik-la <admin@piwik.la>
2014-03-05 01:09:25 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#https://github.com/piwikla/aws-nginx
+
+#switch to root
+sudo -i
+
+#Update
+apt-get update
+apt-get upgrade
+
+#Install Dependencies
+apt-get install build-essential python-software-properties
+
+#Install PHP
+apt-get install php5-fpm php5-mysql php5-mysqlnd php5-gd php5-memcache memcached php5-curl php5-dev php-pear
+
+#Install GeoIP
+apt-get install geoip-bin geoip-database libgeoip-dev
+
+#Install MySQL
+apt-get install mysql-server mysql-client
+
+#Basic Security
+apt-get install fail2ban iptables-persistent
+
+#Install Nginx
+apt-get install nginx
+
+#Update
+apt-get update
+apt-get upgrade
+
+#Start Nginx
+service nginx start
