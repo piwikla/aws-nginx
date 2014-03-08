5846f184d6857cfe505600cfebcbe5b58ddbfdfc
piwik-la <admin@piwik.la>
2014-03-08 11:38:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 #https://github.com/piwikla/aws-nginx
 
 #Update
+add-apt-repository ppa:nginx/stable
+add-apt-repository ppa:ondrej/php5
+add-apt-repository ppa:nijel/phpmyadmin
 apt-get update
 apt-get upgrade
 
@@ -13,7 +16,7 @@ apt-get install unzip
 apt-get install build-essential python-software-properties
 
 #Install PHP
-apt-get install php5-fpm php5-gd php5-memcache memcached php5-curl php5-dev php-pear php5-mysqlnd
+apt-get install php5 php5-fpm php5-gd php5-memcache memcached php5-curl php5-dev php-pear php5-mysqlnd
 
 #Install GeoIP
 apt-get install geoip-bin geoip-database libgeoip-dev
@@ -25,16 +28,9 @@ apt-get install mysql-server mysql-client
 #Install Nginx
 apt-get install nginx
 
-#Start php5-fpm
-service php5-fpm start
-
 #Install phpmyadmin
 apt-get install phpmyadmin
 sudo ln -s /usr/share/phpmyadmin/ /home/piwik/public_html/piwik.la
-
-#Update
-apt-get update
-apt-get upgrade
 
 mkdir -p /home/piwik/public_html/piwik.la
 
@@ -50,6 +46,11 @@ cp /etc/nginx/sites-available/default /etc/nginx/sites-available/piwik.la
 ln -s /etc/nginx/sites-available/piwik.la /etc/nginx/sites-enabled
 rm /etc/nginx/sites-enabled/default
 
+#Update
+apt-get update
+apt-get upgrade
+
+#restart
 service nginx restart
 service php5-fpm restart
 service mysql restart
